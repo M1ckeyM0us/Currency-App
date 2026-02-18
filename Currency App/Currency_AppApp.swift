0789e5d7d0f45537ceaad30aafae8e33c16381ec
M1ckeyM0us <miketeamgamer13@gmail.com>
2026-02-18 08:02:06 -0600

    Initial Commit

---
+++ Currency App/Currency_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_AppApp.swift
+//  Currency App
+//
+//  Created by MYKHAILO NAUMOV on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Currency_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
